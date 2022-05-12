7e7eacb66fa2b023d159c51bfca3c35aa6d87433
stwaldo <stwaldo@gmail.com>
2022-05-12 13:46:43 -0400

    Add task component & story Add snapshot testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stores: ["../src/app/components/**/*.stories.ts"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
